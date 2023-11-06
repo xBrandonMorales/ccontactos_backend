e76e07fcd46dc4392dcb716d3810fabb680528e5
Axel Carrillo <1722110634@utectulancingo.edu.mx>
2023-11-06 16:14:35 +0000

    Ya sirve

---
+++ sql/contactos.sql
@@ -0,0 +1,11 @@
+CREATE TABLE contactos (
+    email text PRIMARY KEY,
+    nombre text,
+    telefono text
+);
+
+INSERT INTO contactos (email, nombre, telefono)
+VALUES ("dejah@email.com", "Dejah Thoris", "111111111");
+
+INSERT INTO contactos (email, nombre, telefono)
+VALUES ("john@eamil.com", "John Carter", "22222222");
