315c655b8efbb32d5ece4ecaa59a4fb69fc89035
Axel Carrillo <1722110634@utectulancingo.edu.mx>
2023-11-13 16:20:30 +0000

    algo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = fastapi.FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://api-contactos-frontend2-eee55bb0cdd1.herokuapp.com"],
+    allow_origins=["https://8080-axelcarrill-herokufront-ha02eb2yfk6.ws-us106.gitpod.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
