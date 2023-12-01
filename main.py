f9f202ea4d77c1f8386156f6b35dc934ebbd7316
Brandon Castro Morales <adrimora.0232@gmail.com>
2023-12-01 05:34:16 +0000

    gg

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = fastapi.FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://heroku-frontend-8bdead322fe2.herokuapp.com"],
+    allow_origins=["https://8080-xbrandonmor-contactofro-dzay1jxbp8r.ws-us106.gitpod.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
